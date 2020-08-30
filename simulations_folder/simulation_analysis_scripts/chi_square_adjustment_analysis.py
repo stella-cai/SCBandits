8b4e70d6366484c021a8b6439e754d66bb32bec4
Ben Prystawski <benpry@users.noreply.github.com>
2020-08-30 16:39:33 -0400

    halfway through updating chi square analysis script

--- simulations_folder/simulation_analysis_scripts/chi_square_adjustment_analysis.py
+++ simulations_folder/simulation_analysis_scripts/chi_square_adjustment_analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 from glob import glob
 import os
+from time import sleep
 import subprocess
 
 def get_all_file_paths(root_dir):
@@ -24,31 +25,33 @@ if __name__ == "__main__":
     os.system("./RunEffectSizeSimsSameArmsTSPPD.sh")
     os.chdir("../simulation_analysis_scripts")
 
-    save_dirs = os.listdir("../simulation_saves")
-    save_dirs = ['../simulation_saves/' + d for d in save_dirs]
+    # wait 10 minutes for all the simulations to finish
+    sleep(600)
+
+    save_dir = 'TSPPDNoEffectResampleFast'
 
     num_sims = 500
     arm_prob = 0.5
     means = {}
     cutoffs = {}
-    for save_dir in save_dirs:
-        for n in (32, 88, 785):
-            try:
-                file_path = glob(save_dir + f"/num_sims={num_sims}armProb={arm_prob}/n={n}/*Df.csv")[0]
-            except IndexError:
-                continue
-            df_sims = pd.read_csv(file_path)
-            plt.hist(df_sims['stat'])
-            plt.title(f"Chi-Square Statistic: n={n}")
-            plt.xlabel("Chi-Square Statistic")
-            plt.ylabel("# Sims")
-            plt.savefig(f'../simulation_analysis_saves/chi_square_histogram_{n}.png')
-            plt.clf()
-            cutoff = df_sims['stat'].sort_values().reset_index()['stat'][475]
-            print(f"cutoff: {cutoff}")
-            cutoffs[f'{n}'] = cutoff
-            print(f"chi square mean: {df_sims['stat'].mean()}")
-            means[f'{n}'] = df_sims['stat'].mean()
+    for n in (32, 88,, 197, 785):
+        for c in (0.025, 0.1, 0.2, 0.3, 0.05)
+        try:
+            file_path = glob(save_dir + f"/num_sims={num_sims}armProb={arm_prob}/N={n}c={c}/*Df.csv")[0]
+        except IndexError:
+            continue
+        df_sims = pd.read_csv(file_path)
+        plt.hist(df_sims['stat'])
+        plt.title(f"Chi-Square Statistic: n={n}")
+        plt.xlabel("Chi-Square Statistic")
+        plt.ylabel("# Sims")
+        plt.savefig(f'../simulation_analysis_saves/chi_square_cutoff/chi_square_histogram_{n}.png')
+        plt.clf()
+        cutoff = df_sims['stat'].sort_values().reset_index()['stat'][475]
+        print(f"cutoff: {cutoff}")
+        cutoffs[f'{n}'] = cutoff
+        print(f"chi square mean: {df_sims['stat'].mean()}")
+        means[f'{n}'] = df_sims['stat'].mean()
 
     # delete all the simulation saves
     for save_dir in save_dirs:
@@ -60,6 +63,9 @@ if __name__ == "__main__":
     for test_script in test_scripts:
         os.system(f"./{test_script}")
     os.chdir("../simulation_analysis_scripts")
+
+    # wait 10 minutes for the new simulations to finish
+    sleep(600)
 
     save_dirs = os.listdir("../simulation_saves")
     save_dirs = ['../simulation_saves/' + d for d in save_dirs]
